dc3a3a167f7c074617bf253fb3b7d4c603cec688
Belen Cholango <belen.cholango@epn.edu.ec>
2025-10-16 22:30:05 -0500

    Add initial implementation of sum functionality in Java

---
+++ suma.java
@@ -0,0 +1,16 @@
+// Lógica para sumar dos números en Java
+
+public class suma {
+
+    //Método que devuelve la suma de dos números.
+    public static int sumar(int a, int b) {
+        return a + b;
+    }
+
+    // Método principal para probar la función
+    public static void main(String[] args) {
+        int resultado = sumar(2, 3);
+        System.out.println("Prueba de suma:");
+        System.out.println("2 + 3 = " + resultado);
+    }
+}
